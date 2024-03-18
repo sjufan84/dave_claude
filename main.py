5ec88af746e0d61038684738c05d0b85a3aca9f7
David Thomas <dave_thomas@enoughwebapp.com>
2024-03-17 22:08:21 -0500

    fixed file uploads, added new intro message

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from typing import List
 import logging
+import markdown
 from anthropic import Anthropic, MessageStream
 from pydantic import BaseModel, Field
 import base64
+from PIL import Image
 from io import BytesIO
 from utils.extraction_utils import (
     extract_docx_file_contents, extract_text_file_contents, extract_pdf_file_contents
@@ -17,14 +19,6 @@ client = Anthropic(api_key=st.secrets["my_secrets"]["ANTHROPIC_KEY"])
 default_initial_message = """ You are a helpful assistant helping the user answer questions\
     and analyze files. """
 
-initial_uploaded_file_message = [
-    {
-        "role": "user",
-        "content": "I am uploading some files that I would like to ask you some questions about.\
-            These may be image files, text files, or some combination of both."
-    }
-]
-
 # Initialize session state
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
@@ -33,24 +27,40 @@ if "initial_message" not in st.session_state:
     the user answer questions and analyze files."
 if "is_logged_in" not in st.session_state:
     st.session_state.is_logged_in = False
-if "file_chat_messages" not in st.session_state:
-    st.session_state.file_chat_messages = []
 if "uploaded_files" not in st.session_state:
     st.session_state.uploaded_files = []
-
+if "filesUploaded" not in st.session_state:
+    st.session_state.filesUploaded = False
+if "currentFileType" not in st.session_state:
+    st.session_state.currentFileType = None
+if "current_string" not in st.session_state:
+    st.session_state.current_string = None
+if "isNewSession" not in st.session_state:
+    st.session_state.isNewSession = True
 
 class VisionMessageRequest(BaseModel):
     file_type: str = Field(..., description="The type of file uploaded. Must be one of\
      JPEG, JPG, PNG, GIF, or WebP")
     image_content: str = Field(..., description="The image encoded as a base64 string")
+    message_content: str = Field(..., description="The message content to send to the assistant")
 
 def encode_image(image_file: BytesIO) -> str:
+    # Resize the image to 512x512
+    image = Image.open(image_file)
+    image = image.resize((512, 512))
+    image_file = BytesIO()
+    image.save(image_file, format="PNG")
+    image_file.seek(0)
+
     return base64.b64encode(image_file.read()).decode()
 
 def get_initial_message(initial_prompt: str) -> str:
     return f""" The user has specified that they would like to initiate the chat with
     the following system message {initial_prompt}.  Based on this, adapt your style and approach
-    to the ensuing conversation. """
+    to the ensuing conversation.  Return all of your answers in markdown format in the format
+    that makes the most sense to you given the context.  If the user asks for specific formatting,
+    please adhere to their request.  Your respones will be converted to HTML so make sure to
+    use markdown syntax."""
 
 def password_check():
     password = st.text_input("Enter your password", type="password")
@@ -81,24 +91,25 @@ def create_vision_message(message_params: VisionMessageRequest) -> List[dict]:
             },
             {
                 "type": "text",
-                "text": "Can you help me answer\
-                some questions about this image?"
+                "text": message_params.message_content
             }
         ],
     }
 
     return image_message
 
-def create_text_message(file_text: str) -> List[dict]:
+def create_text_message(file_text: str, message_content: str) -> dict:
     text_message = {
-        "role" : "user", "content" : f"Can you help me answer questions\
-            about this text that I have uploaded? {file_text}"
+        "role" : "user", "content" : f"I have uploaded some text files {file_text}.\
+        Can you help answer my questions about them? {message_content}"
     }
 
     return text_message
 
 def file_upload_handler(uploaded_files):
+    st.session_state_filesUploaded = True
     if uploaded_files is not None:
+        total_text = ""
         for file in uploaded_files:
             # If the file is already in the session state, skip it
             if file.name in st.session_state.uploaded_files:
@@ -112,33 +123,25 @@ def file_upload_handler(uploaded_files):
                     st.write(f"Extracting text from pdf file: {file.name}")
                     extracted_text = extract_pdf_file_contents(file_contents)
                     st.write(f"Extracted text from pdf: {extracted_text}")
-                    st.session_state.file_chat_messages.append(create_text_message(extracted_text))
-                    st.write(f"PDF text appended to chat messages: {st.session_state.file_chat_messages}")
+                    total_text += extracted_text
                 elif file_type == "docx":
                     st.write(f"Extracting text from docx file: {file.name}")
                     extracted_text = extract_docx_file_contents(file_contents)
                     st.write(f"Extracted text from docx: {extracted_text}")
-                    st.session_state.file_chat_messages.append(create_text_message(extracted_text))
-                    st.write(f"Docx text appended to chat messages: {st.session_state.file_chat_messages}")
+                    total_text += extracted_text
                 elif file_type == "txt":
                     st.write(f"Extracting text from txt file: {file.name}")
                     extracted_text = extract_text_file_contents(file_contents)
                     st.write(f"Extracted text from txt: {extracted_text}")
-                    st.session_state.file_chat_messages.append(create_text_message(extracted_text))
-                    st.write(f"Txt text appended to chat messages: {st.session_state.file_chat_messages}")
-            elif file_type in ["jpg", "png", "jpg", "jpeg"]:
+                    total_text += extracted_text
+                st.session_state.current_string = total_text
+                st.session_state.currentFileType = "text"
+            elif file_type in ["webP", "png", "jpg", "jpeg", "gif"]:
                 st.write(f"Handling image file of type {file_type}")
                 file_contents = file.read()
                 file_base64 = encode_image(BytesIO(file_contents))
-                st.session_state.file_chat_messages.append(
-                    create_vision_message(
-                        VisionMessageRequest(
-                            file_type=file_type,
-                            message_content="",
-                            image_content=file_base64
-                        )
-                    )
-                )
+                st.session_state.current_string = file_base64
+                st.session_state.currentFileType = file_type
                 st.write("Image file appended to chat messages")
 
             else:
@@ -147,35 +150,56 @@ def file_upload_handler(uploaded_files):
         st.warning("Please upload a file")
 
 def main():
-    st.sidebar.markdown(":blue[Upload files to chat with Claude about:]")
-    uploaded_files = None
-    uploaded_files = st.sidebar.file_uploader(
-        "Upload files", type=["pdf", "docx", "txt", ".png", ".jpg", ".jpeg"],
-        label_visibility="collapsed", accept_multiple_files=True,
-    )
-    if uploaded_files:
-        upload_files_button = st.sidebar.button(
-            ":blue[Upload files]", type="secondary", use_container_width=True, key="upload_files_button"
+    if st.session_state.isNewSession:
+        st.markdown("""Meet Claude, the cutting-edge Language Model (LLM)
+        that surpasses GPT-4 in capability and knowledge. Engage in deep conversations
+        on a wide range of topics, as Claude can track up to 350 pages of text.
+        A standout feature is the ability to upload documents (and soon images)
+        for Claude to analyze, summarize, explain, or help you understand them better.
+        You can upload one document at a time and ask questions before moving on to another.
+        Claude will remember all documents uploaded per session, so start a new session
+        for unrelated topics. With a memory capacity of over 300 pages,
+        feel free to explore the limits of Claude's abilities.\n\nOne other fun
+        thing to play around with is the 'Initial Message' feature.  This is what shapes
+        how Claude responds to you.  So, for instance, you may want him to respond as if
+        he was your advisor, a friend, a teacher, or even a character from a movie (pirates area always
+        fun).  If you want to change the initial message, click the button on the sidebar and
+        enter a new message\n\nAs this is a new system, please report
+        any bugs encountered. Enjoy your experience with Claude!""")
+    if st.session_state.filesUploaded:
+        st.sidebar.markdown(":red[You have already uploaded a file to the chat.\
+            Please ask a question and then you will be able to upload more files.]")
+    elif not st.session_state.filesUploaded:
+        st.sidebar.markdown(":blue[Upload files to chat with Claude about:]")
+        uploaded_files = st.sidebar.file_uploader(
+            "Upload files", type=["pdf", "docx", "txt"],
+            label_visibility="collapsed"
         )
-        if upload_files_button:
-            with st.spinner("Processing files..."):
-                st.write("Processing files...")
-                file_upload_handler(uploaded_files)
-                for file in uploaded_files:
-                    # Check to make sure that the file is not already in the session state
-                    if file.name not in st.session_state.uploaded_files:
-                        st.session_state.uploaded_files.append(file.name)
-                        st.write(f"Uploaded files: {st.session_state.uploaded_files}")
-                uploaded_files = None
-                st.rerun()
+        if uploaded_files:
+            upload_files_button = st.sidebar.button(
+                ":blue[Upload files]", type="secondary", use_container_width=True, key="upload_files_button"
+            )
+            if upload_files_button:
+                # Check to make sure there aren't multiple image files uploaded
+                with st.spinner("Processing files..."):
+                    st.write("Processing files...")
+                    file_upload_handler([uploaded_files])
+                    for file in [uploaded_files]:
+                        # Check to make sure that the file is not already in the session state
+                        if file.name not in st.session_state.uploaded_files:
+                            st.session_state.uploaded_files.append(file.name)
+                            st.write(f"Uploaded files: {st.session_state.uploaded_files}")
+                    st.session_state.filesUploaded = True
+                    uploaded_files = None
+                    st.rerun()
     if st.session_state.uploaded_files != []:
-        st.sidebar.markdown("**:blue[Uploaded Files:]**")
+        st.sidebar.markdown("**:blue[Uploaded files for this session:]**")
         for file in st.session_state.uploaded_files:
             st.sidebar.write(file)
     st.sidebar.markdown("---")
 
     adjust_initial_message_button = st.sidebar.button(
-        ":violet[Adjust initial message]", type="secondary", use_container_width=True
+        ":blue[Adjust initial message]", type="secondary", use_container_width=True
     )
     if adjust_initial_message_button:
         new_message = st.sidebar.text_area(
@@ -187,13 +211,30 @@ def main():
         if submit_message_button:
             st.session_state.initial_message = get_initial_message(new_message)
             st.sidebar.success(f"Initial message updated to {new_message}")
+            st.rerun()
+    start_new_session_button = st.sidebar.button(
+        ":red[Start a new session]", type="secondary", use_container_width=True
+    )
+    if start_new_session_button:
+        st.session_state.chat_history = []
+        st.session_state.initial_message = default_initial_message
+        st.session_state.filesUploaded = False
+        st.session_state.uploaded_files = []
+        st.success("New session started. Chat history and initial message reset.")
+        st.rerun()
 
     # Display chat messages from history on app rerun
     for message in st.session_state.chat_history:
         logging.debug(f"Displaying message: {message}")
         if message["role"] == "assistant":
             with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+                try:
+                    message = markdown.markdown(message["content"])
+                    st.markdown("""<div>""" + message + """</div>""", unsafe_allow_html=True)
+                except Exception as e:
+                    st.error(f"Error displaying message: {e}")
+                    st.markdown(message["content"])
+
         elif message["role"] == "user":
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
@@ -201,8 +242,28 @@ def main():
     if prompt := st.chat_input("Hey friend, how can I help you today?"):
         logging.info(f"Received user input: {prompt}")
         # Add user message to chat history
-        st.session_state.chat_history.append({"role": "user", "content": prompt})
-        st.write(f"Chat history: {st.session_state.chat_history}")
+        if st.session_state.filesUploaded and st.session_state.currentFileType != "text":
+            st.session_state.chat_history.append(
+                create_vision_message(
+                    VisionMessageRequest(
+                        file_type=st.session_state.currentFileType,
+                        image_content=st.session_state.current_string,
+                        message_content=prompt
+                    )
+                )
+            )
+        elif st.session_state.filesUploaded and st.session_state.currentFileType == "text":
+            st.session_state.chat_history.append(
+                create_text_message(st.session_state.current_string, prompt)
+            )
+        else:
+            st.session_state.chat_history.append({"role": "user", "content": prompt})
+            st.write(f"Chat history: {st.session_state.chat_history}")
+
+        st.session_state.current_string = None
+        st.session_state.currentFileType = None
+        st.session_state.filesUploaded = False
+        uploaded_files = None   
 
         with st.chat_message("user"):
             st.markdown(prompt)
@@ -220,37 +281,19 @@ def main():
                         full_response += event.delta.text
                         message_placeholder.markdown(full_response + "▌")
 
-            # Start the streaming conversation
-            # Filter the chat history to be the last 8 messages
-            # as long as the first message has role user
-            if len(st.session_state.file_chat_messages) > 0:
-                st.session_state.chat_history_messages = initial_uploaded_file_message + st.session_state.file_chat_messages
-                messages = st.session_state.chat_history + st.session_state.file_chat_messages
-                st.write(f"File chat messags {st.session_state.file_chat_messages}\
-                added to chat history")
-            else:
-                messages = st.session_state.chat_history
-
-            st.stop()
-
             with client.messages.stream(
-                messages=messages,
+                messages=st.session_state.chat_history,
                 model="claude-3-opus-20240229",
                 event_handler=StreamHandler,
                 system = st.session_state.initial_message,
                 max_tokens=3500
             ) as stream:
-                st.write(f"Current initial message: {st.session_state.initial_message}")
                 final_message = stream.get_final_message()
                 message_placeholder.markdown(final_message.content[0].text)
                 st.session_state.chat_history.append(
                     {"role" : "assistant", "content" : final_message.content[0].text}
                 )
                 logging.debug(f"Chat history: {st.session_state.chat_history}")
-                # Reset the file chat messages
-                st.session_state.file_chat_messages = []
-                # Reset the uploaded files
-                uploaded_files = None
 
 if __name__ == "__main__":
     if st.session_state.is_logged_in:
